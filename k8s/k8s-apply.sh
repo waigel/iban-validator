60d7ae5b24c06a6fe2d46abad9debc6870c4dd10
Johannes Waigel <waigel@klexhub.com>
2023-01-12 01:21:18 +0100

    :rocket: add k8s deployment

---
+++ k8s/k8s-apply.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+export APP_NAME="iban-validator"
+export NAMESPACE="lexoffice"
+
+export FRONTEND_IMAGE="eu.gcr.io/$PROJECT_ID/$APP:latest"
+export IMAGE="waigel.azurecr.io/lexoffice/iban-validator"
+export DOMAIN="iban-validator.waigel.com"
+export VERSION=$(git rev-parse --short HEAD)
+
+kubectl kustomize k8s/template | envsubst > k8s/prod.yml
+kubectl apply -f k8s/prod.yml
